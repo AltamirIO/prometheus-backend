f20af8cfbeb73ff9ad7bd655db4731b1d9408123
Michel Paupulaire <mpaupulaire4@gmail.com>
2018-06-07 11:50:54 -0600

    beginning

---
+++ src/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config({silent: true})
+
+import express from 'express'
+import cookie from 'cookie'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import compression from 'compression'
+import bodyParser from 'body-parser'
+import { createServer } from 'http'
+
+import CMSAuth from  './githubCMSAuth'
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(compression())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+CMSAuth(app)
+
+// Server any static files
+// app.use(express.static('public'))
+
+const server = createServer(app);
+
+server.listen(port, () => {
+  console.log(`API Server is now running on port ${port}`);
+})
